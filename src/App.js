5c42eae46bffa766e136092ff8a5342834917916
DeynerNB <deynernavarrob@gmail.com>
2024-06-04 21:51:44 -0600

    Implement elements as a object

--- src/App.js
+++ src/App.js
@@ -6,23 +6,35 @@ import CodeDisplay from './components/configurationPanel/CodeDisplay';
 import './App.css';
 
 function App() {
-    const [elements, setElements] = useState([]);
-    const [selectedElement, setSelectedElement] = useState(null);
+    const [elements, setElements] = useState({});
+    const [selectedElementKey, setSelectedElementKey] = useState(null);
     const [showCodeDisplay, setShowCodeDisplay] = useState(false);
 
-    const addElement = (type) => {
-        let count = elements.filter( (elem) => elem.type.name == type.name ).length
-        setElements([...elements, { type: { ...type, ID: `${type.name}_${count}` } }]);
+    const addElement = (new_element) => {
+        let count = 0
+        Object.keys( elements ).forEach( key => {
+            count += key.includes( new_element.name ) ? 1 : 0;
+        });
+
+        const element_id = `${new_element.name}_${count}`
+        const updatedElements = {
+            ...elements,
+            [element_id]: new_element
+        }
+
+        setElements(updatedElements);
     };
 
-    const selectElement = (index) => {
-        setSelectedElement(index);
+    const selectElement = (element_key) => {
+        setSelectedElementKey(element_key);
     };
 
     const updateConfig = (newConfig) => {
-        const updatedElements = elements.map((element, index) =>
-            index === selectedElement ? { type: { ...element.type, config: newConfig } } : element
-        );
+        const updatedElements = {
+            ...elements,
+            [selectedElementKey]: { ...elements[selectedElementKey], config: newConfig }
+        }
+
         setElements(updatedElements);
     };
 
@@ -37,10 +49,10 @@ function App() {
                     <ElementPanel onAddElement={addElement} />
                 </div>
                 <div className='col-8 panel-tkinter'>
-                    <Canvas elements={elements} onSelectElement={selectElement} onElementMoveConfig={updateConfig} selectedElementIndex={selectedElement}/>
+                    <Canvas elements={elements} setSelectElementKey={selectElement} onElementMoveConfig={updateConfig} selectedElementKey={selectedElementKey}/>
                 </div>
                 <div className='col pt-2 panel-tkinter '>
-                    <ConfigPanel element={elements[selectedElement]} onUpdateConfig={updateConfig} codeDisplayDialog={codeDisplayDialog} />
+                    <ConfigPanel element={ elements[ selectedElementKey ] } onUpdateConfig={updateConfig} codeDisplayDialog={codeDisplayDialog} />
                 </div>
             </div>
 
