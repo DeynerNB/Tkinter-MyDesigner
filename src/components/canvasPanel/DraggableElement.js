a56cfe502f1022fffcc482975ecd059835172e2d
DeynerNB <deynernavarrob@gmail.com>
2024-06-05 13:17:59 -0600

    Fixed element out of the canvas

--- src/components/canvasPanel/DraggableElement.js
+++ src/components/canvasPanel/DraggableElement.js
@@ -4,8 +4,15 @@ import "./DraggableStyle.css"
 
 function DraggableElement({ element, isSelected, onElementMoveConfig }) {
     const [offset, setOffset] = useState({ x: 0, y: 0 });
-    const element_panel = document.getElementById("element-panel")
 
+    const element_panel = document.getElementById("element-panel")
+    const canvas_panel = document.getElementById("canvas-panel")
+
+    const handleDrag = (e) => {
+        e.preventDefault();
+    };
+
+    // Calculate the initial offset
     const handleDragStart = (e) => {
         const rect = e.target.getBoundingClientRect();
         setOffset({
@@ -14,39 +21,64 @@ function DraggableElement({ element, isSelected, onElementMoveConfig }) {
         });
     };
 
-    const handleDrag = (e) => {
-        e.preventDefault();
-    };
+    const calculateElementPosition = (e) => {
+        const offset_element = 20 + 5;
+        const leftUp_corner_pos  = e.clientX - element_panel.offsetWidth - offset.x;
+        const rightUp_corner_pos = leftUp_corner_pos + element.config.width + offset_element
 
+        console.log(element.config)
+        console.log(rightUp_corner_pos)
+
+        let positionX = 0;
+        if (leftUp_corner_pos <= 0) { positionX = 0; }
+        else if (rightUp_corner_pos >= canvas_panel.offsetWidth) { positionX = canvas_panel.offsetWidth - element.config.width - offset_element }
+        else { positionX = leftUp_corner_pos }
+
+        // const positionX = (temp_posX <= 0) ? 0 : temp_posX;
+        const positionY = e.clientY - offset.y
+
+        return { positionX, positionY }
+    }
+
+    // Update the element new position
     const handleDragEnd  = (e) => {
+        // If the element is not selected := Cannot be drag
         if (!isSelected) {
             return;
         }
+        // Calculate the new position relative to the grab point
+        const temp_posX = e.clientX - element_panel.offsetWidth - offset.x;
+        const { positionX, positionY } = calculateElementPosition( e )
 
-        const positionX = (e.clientX - element_panel.offsetWidth <= 0) ? 0 : e.clientX - element_panel.offsetWidth - offset.x;
-        const positionY = e.clientY - offset.y
+        // const positionX = (temp_posX <= 0) ? 0 : temp_posX;
+        // const positionY = e.clientY - offset.y
 
+        // Set the element position configuration
         const new_config = {
             ...element.config,
-            "posX": positionX.toFixed(2),
-            "posY": positionY.toFixed(2),
+            "posX": Number( positionX.toFixed(2) ),
+            "posY": Number( positionY.toFixed(2) ),
         }
 
+        // Update the element configuration
         onElementMoveConfig( new_config )
     };
 
     return (
         <div
             className={`draggable-${element.name} text-center text-nowrap ${ isSelected ? "selected_element" : "" }`}
-            style={{ left: Number(element.config.posX), top: Number(element.config.posY), position: 'absolute', width: `${element.config.width}px`, height: `${element.config.height}px` }}
+            style={{
+                left: Number(element.config.posX),
+                top: Number(element.config.posY),
+                position: 'absolute',
+                width: `${element.config.width}px`,
+                height: `${element.config.height}px` }}
             draggable
             onDragStart={handleDragStart}
             onDrag={handleDrag}
             onDragEnd={handleDragEnd}
         >
-            {
-                element.config.textContent || element.name
-            }
+            { element.config.textContent || element.name }
         </div>
     );
 }
